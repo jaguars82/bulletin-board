3a085ceee4a85eb3c05c9e895e8773f36d338a5e
Egor Sechin <jaguars82@yandex.ru>
2019-12-17 18:41:09 +0300

    Saving changes to some pages in 'components' directory

---
+++ src/components/Ads/Ad.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12>
+        <f1>Ad</f1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
