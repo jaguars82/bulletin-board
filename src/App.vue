361518c0d4c7947b75d131eaad0e3699322a48b6
Egor Sechin <jaguars82@yandex.ru>
2019-12-17 15:57:48 +0300

    Initial commit of the project

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-app>
+
+    <v-navigation-drawer
+      app
+      temporary
+      v-model="drawer"
+    >
+
+    <v-list>
+      <v-list-item
+        v-for="link in links"
+        :key="link.title"
+        :to="link.url"
+      >
+        <v-list-item-icon>
+          <v-icon >{{link.icon}}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="link.title"></v-list-item-title>
+        </v-list-item-content>
+
+      </v-list-item>
+    </v-list>
+
+    </v-navigation-drawer>
+
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon @click="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
+      <v-toolbar-title>Ad application</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-sm-and-down">
+
+        <v-btn
+          text
+          v-for="link of links"
+          :key="link.title"
+          :to="link.url"
+        >
+          <v-icon left>{{link.icon}}</v-icon>
+          {{link.title}}
+        </v-btn>
+
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-content>
+
+        <router-view></router-view>
+
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+  components: {
+
+  },
+
+  data () {
+    return {
+      drawer: false,
+      links: [
+        {title: 'Login', icon: 'mdi-lock', url: '/login'},
+        {title: 'Registration', icon: 'mdi-face', url: '/registration'},
+        {title: 'Orders', icon: 'mdi-bookmark-outline', url: '/orders'},
+        {title: 'New ad', icon: 'mdi-note-plus-outline', url: '/new'},
+        {title: 'My ads', icon: 'mdi-format-list-bulleted', url: '/list'},
+      ]
+    }
+  }
+}
+</script>
