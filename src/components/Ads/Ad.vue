1df20ff3d6efcc91745986da3448d57007d7dac9
Egor Sechin <jaguars82@yandex.ru>
2019-12-28 21:50:22 +0300

    Complete creation of UI pages

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,10 +1,19 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Ad</h1>
-      </v-flex>
-    </v-layout>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-img src="https://cdn.vuetifyjs.com/images/carousel/sky.jpg" height="300px"></v-img>
+          <v-card-title>Lorem</v-card-title>
+          <v-card-text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto, possimus!</v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn class="warning" text>Edit</v-btn>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,17 +1,62 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Ad List</h1>
-      </v-flex>
-    </v-layout>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="6">
+
+        <h1 class="text--secondary mb-3">My ads</h1>
+
+        <v-card class="elevation-10 mb-3" v-for="ad in ads" :key="ad.id">
+          <v-row>
+            <v-col class="py-0" cols="4">
+              <v-img
+                :src="ad.imageSrc"
+                height="180px"
+              ></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card-title>
+                {{ ad.title }}
+              </v-card-title>
+
+              <v-card-text>{{ ad.description }}</v-card-text>
+
+              <v-card-actions>
+
+                <v-spacer></v-spacer>
+
+                <v-btn class="info" :to="'ad/' + ad.id">Open</v-btn>
+
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      ads: [
+        {
+          title: 'First ad',
+          description: 'Hello! I am 1st description!',
+          promo: false,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          id: '123'
+        },
+        {
+          title: 'Second ad',
+          description: 'Hello! I am 2nd description!',
+          promo: true,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          id: '123243'
+        }
+      ]
+    }
   }
 }
 </script>
